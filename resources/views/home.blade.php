fe99e174887312d07b76c705e6218e2185bdbe01
Steveh77 <stederosa76@gmail.com>
2022-09-22 14:14:05 +0200

    Added navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.main')
 
 @section('main-section')
+    <h1 class="text-center py-5 text-white">HOME</h1>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,22 @@
 
 <body>
     <header>
-
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"><img class="img-fluid m-1"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png"
+                    alt="" style="
+                    width: 50px;"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="{{ route('home') }}">Home</span></a>
+                    <a class="nav-item nav-link" href="{{ route('books.index') }}">Books</a>
+                </div>
+            </div>
+        </nav>
     </header>
     <main>
         @yield('main-section')
